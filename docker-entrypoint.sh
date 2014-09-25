0d2408580d4288ff9ebd6567e2fffaf4ab310e06
Brian Claywell <bclaywel@fhcrc.org>
2014-09-25 14:29:28 -0700

    If the export directory doesn't exist, create it.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 # Configure exports.
 if [ -n "${DATA_EXPORTS}" -a -n "${DATA_EXPORT_DIR}" ]; then
+    [ -d ${DATA_EXPORT_DIR} ] || (mkdir -p ${DATA_EXPORT_DIR} && chown shiny:shiny ${DATA_EXPORT_DIR})
+
     su -c "docker-link-exports" shiny
 fi
 
