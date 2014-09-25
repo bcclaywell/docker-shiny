e6e8e44ab58b49bf3341a52a23672b50b4237bf1
Brian Claywell <bclaywel@fhcrc.org>
2014-09-25 14:27:42 -0700

    Sync docker-link-exports again.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Configure exports.
 if [ -n "${DATA_EXPORTS}" -a -n "${DATA_EXPORT_DIR}" ]; then
-    docker-link-exports
+    su -c "docker-link-exports" shiny
 fi
 
 # Replace this shell with the supplied command (and any arguments).
